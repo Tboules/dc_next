d81ae6b6b260ce49f7e072a13f696a56c5def73d
tmitboules <88787579+tmitboules@users.noreply.github.com>
2022-12-17 11:52:53 -0800

    tailwind ui and inter font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,20 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 
+import { Inter } from "@next/font/google";
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({ subsets: ["latin"] });
+
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
     </SessionProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,42 +8,74 @@ interface Props {
   romans: Prisma.NkjvMinAggregateOutputType;
 }
 
-export default function Home({ bookInfo, romans }: Props) {
+const people = [
+  {
+    name: "Calvin Hawkins",
+    email: "calvin.hawkins@example.com",
+    image:
+      "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  {
+    name: "Kristen Ramos",
+    email: "kristen.ramos@example.com",
+    image:
+      "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+  {
+    name: "Ted Fox",
+    email: "ted.fox@example.com",
+    image:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+  },
+];
+
+export default function Home() {
   return (
     <div>
-      <h1 className="text-7xl font-bold underline">Hello world!</h1>
+      <ul className="divide-y divide-gray-200">
+        {people.map((person) => (
+          <li key={person.email} className="py-4 flex">
+            <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
+            <div className="ml-3">
+              <p className="text-sm font-medium text-gray-900">{person.name}</p>
+              <p className="text-sm text-gray-500">{person.email}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+      {/* <h1 className="text-7xl font-bold underline">Hello world!</h1>
       <pre>{JSON.stringify(romans, null, 2)}</pre>
-      <pre>{JSON.stringify(bookInfo, null, 2)}</pre>
+      <pre>{JSON.stringify(bookInfo, null, 2)}</pre> */}
     </div>
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
-  const session = await getServerAuthSession({ req, res });
+// export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+//   const session = await getServerAuthSession({ req, res });
 
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/api/auth/signin",
-        permanent: false,
-      },
-    };
-  }
+//   if (!session) {
+//     return {
+//       redirect: {
+//         destination: "/api/auth/signin",
+//         permanent: false,
+//       },
+//     };
+//   }
 
-  const bookInfo = await prisma.book_info.findMany();
-  const romans = await prisma.nkjv.findMany({
-    where: {
-      book: 45,
-      chapter: 5,
-    },
-    include: {
-      book_info: {
-        select: {
-          title: true,
-        },
-      },
-    },
-  });
+//   const bookInfo = await prisma.book_info.findMany();
+//   const romans = await prisma.nkjv.findMany({
+//     where: {
+//       book: 45,
+//       chapter: 5,
+//     },
+//     include: {
+//       book_info: {
+//         select: {
+//           title: true,
+//         },
+//       },
+//     },
+//   });
 
-  return { props: { bookInfo, romans, session } };
-};
+//   return { props: { bookInfo, romans, session } };
+// };

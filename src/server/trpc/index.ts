e629ddd04e28196bd175d7728037d74b8fae7a41
tmitboules <88787579+tmitboules@users.noreply.github.com>
2022-12-29 20:39:30 -0800

    init trpc

---
+++ src/server/trpc/index.ts
@@ -0,0 +1,27 @@
+import { TRPCError, initTRPC } from "@trpc/server";
+import { Context } from "./context";
+import superjson from "superjson";
+
+const t = initTRPC.context<Context>().create({
+  transformer: superjson,
+  errorFormatter({ shape }) {
+    return shape;
+  },
+});
+// Base router and procedure helpers
+export const router = t.router;
+export const publicProcedure = t.procedure;
+
+const isAuthed = t.middleware(({ ctx, next }) => {
+  if (!ctx.session || !ctx.session.user) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+  return next({
+    ctx: {
+      // infers the `session` as non-nullable
+      session: { ...ctx.session, user: ctx.session.user },
+    },
+  });
+});
+
+export const protectedProcedure = t.procedure.use(isAuthed);

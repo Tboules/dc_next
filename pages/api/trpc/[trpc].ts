ccf005ccb6fa8833b0fffbda2e99599f71bd81d8
tmitboules <88787579+tmitboules@users.noreply.github.com>
2022-12-29 20:47:44 -0800

    starting with trpc -- need to create routers

---
+++ pages/api/trpc/[trpc].ts
@@ -0,0 +1,15 @@
+import { createNextApiHandler } from "@trpc/server/adapters/next";
+import { createContext } from "../../../src/server/trpc/context";
+import { appRouter } from "../../../src/server/trpc/router/_app";
+
+// export API handler
+export default createNextApiHandler({
+  router: appRouter,
+  createContext,
+  // onError:
+  //   env.NODE_ENV === "development"
+  //     ? ({ path, error }) => {
+  //         console.error(`❌ tRPC failed on ${path}: ${error}`);
+  //       }
+  //     : undefined,
+});

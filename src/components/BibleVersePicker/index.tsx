53e623369c0df0afeed5b6da58e419c79dc6e641
tmitboules <88787579+tmitboules@users.noreply.github.com>
2022-12-30 16:59:19 -0800

    working with stuff

--- src/components/BibleVersePicker/index.tsx
+++ src/components/BibleVersePicker/index.tsx
@@ -1,4 +1,5 @@
 import { trpc } from "../../utils/trpc";
+import AutoSelectDropDown from "../AutoSelectDropDown";
 
 export default function Example() {
   const { data: bookInfo } = trpc.bible.getAllBookInfo.useQuery();
@@ -14,32 +15,17 @@ export default function Example() {
   return (
     <div className="bg-white shadow sm:rounded-lg">
       <div className="px-4 py-5 sm:p-6">
-        <h3 className="text-lg font-medium leading-6 text-gray-900">
-          Update your email
-        </h3>
-        <div className="mt-2 max-w-xl text-sm text-gray-500">
+        <div className="flex justify-between">
+          <h3 className="text-lg font-medium leading-6 text-gray-900">
+            Find a verse from the Bible
+          </h3>
+          <form action="">
+            <AutoSelectDropDown />
+          </form>
+        </div>
+        <div className="mt-4 max-w-xl text-md text-gray-700">
           <p>Change the email address you want associated with your account.</p>
         </div>
-        <form className="mt-5 sm:flex sm:items-center">
-          <div className="w-full sm:max-w-xs">
-            <label htmlFor="email" className="sr-only">
-              Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-              placeholder="you@example.com"
-            />
-          </div>
-          <button
-            type="submit"
-            className="mt-3 inline-flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-          >
-            Save
-          </button>
-        </form>
       </div>
     </div>
   );
